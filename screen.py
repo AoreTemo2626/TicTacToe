1ab41ab28c27659913c8cb9ee7341d15ebe7e795
AoreTemo <denshpk@gmail.com>
2023-01-05 01:15:06 +0200

    for screen size vatiables

---
+++ screen.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from forroot import root
+
+width = 600;
+height = 500;
+
+screen_width = root.winfo_screenwidth();
+screen_height = root.winfo_screenheight();
+
+center_x = int((screen_width - width)/2);
+center_y = int((screen_height - height)/2);
