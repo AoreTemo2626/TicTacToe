cdc94328f6ef047316b5b7ea32854c82f8b615c7
AoreTemo <denshpk@gmail.com>
2023-01-05 21:53:45 +0200

    start of gameplay creating

--- buttons.py
+++ buttons.py
@@ -1,12 +1,27 @@
 from forroot import *
+from screen import *
 
-for c in range(3): root.columnconfigure(index = c, weight = 1);
-for r in range(3): root.rowconfigure(index = r, weight = 1);
+b = True;
 
+def mf(event):
+	global b;
+	button = event.widget;
+	if b:
+		button['text'] = 'X';
+	else:
+		button['text'] = 'O';
 
+	b = not b;
 
+for c in range(3): frame.columnconfigure(index = c, weight = 1);
+for r in range(3): frame.rowconfigure(index = r, weight = 1)
 for r in range(3):
     for c in range(3):
-        btn = ttk.Button()
+        btn = Button(frame, text = ' ', width = 16, height = 8, font = ("Arial", 30))
         btn.grid(row=r, column=c)
- 
+        btn.bind("<Button-1>", mf)
+
+
+
+
+
--- forroot.py
+++ forroot.py
@@ -1,3 +1,5 @@
 from tkinter import *
 from tkinter import ttk
 root = Tk();
+frame = ttk.Frame(root);
+frame.pack();
