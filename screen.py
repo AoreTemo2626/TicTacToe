a4f0d7336b4af1825dc46a65ea1391e0d9ea480e
AoreTemo <denshpk@gmail.com>
2023-01-05 20:38:18 +0200

    some changes

--- screen.py
+++ screen.py
@@ -1,9 +1,9 @@
 from forroot import *
-width = 600;
-height = 500;
+w_width = 600;
+w_height = 400;
 
 screen_width = root.winfo_screenwidth();
 screen_height = root.winfo_screenheight();
 
-center_x = int((screen_width - width)/2);
-center_y = int((screen_height - height)/2);
+center_x = int((screen_width - w_width)/2);
+center_y = int((screen_height - w_height)/2);
